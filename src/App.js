54ba9803eac6b4a3fcd6c641a71684a5c39c66c7
Harshit Sachan <sachanharshit80@gmail.com>
2024-05-29 09:39:39 +0530

    First

--- src/App.js
+++ src/App.js
@@ -24,3 +24,16 @@ export default App;
 // default export 
 // in one js file you have default export 
 // and multiple named exports
+
+
+
+// first we will make navabar , inside it we will have a div for left side , and a div fro right side .
+// in the right side div , we will add four 5 heading , . in the right side div we will add space between . all the headings . 
+
+//  navbar can be used outside the header and indisde the header
+// navbar is used to navigate to pages 
+
+
+
+// Navbar Component
+// use a ul as a container and there will be 4 li 
